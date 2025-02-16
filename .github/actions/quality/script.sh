48b015137f0b94654ade8b49cb64b6cc1e777884
Paulo Silva Barros <paulo.sb@live.com>
2025-02-16 20:04:19 -0300

    interacting with sonarqube

--- .github/actions/quality/script.sh
+++ .github/actions/quality/script.sh
@@ -2,4 +2,16 @@
 
 # Author: Paulo S. Barros
 
-echo "Assuring Quality"
+#REQUIRES:
+#   SONARQUBE_URL
+#   SONARQUBE_TOKEN
+#   PROJECT_KEY
+
+# Sonar configuration:
+export SONAR_HOST_URL=$SONARQUBE_URL
+
+echo "Getting pysonar-scanner"
+pip install pysonar-scanner
+
+echo "Scanning"
+pysonar-scanner -Dsonar.token=$SONARQUBE_TOKEN -DprojectKey=$PROJECT_KEY
