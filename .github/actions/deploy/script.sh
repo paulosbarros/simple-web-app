1263a1443a7700488313dca3f5e64a540694d68b
Paulo Silva Barros <paulo.sb@live.com>
2025-02-17 11:16:43 -0300

    change env name

--- .github/actions/deploy/script.sh
+++ .github/actions/deploy/script.sh
@@ -7,8 +7,8 @@
 #   GITHUB_REPOSITORY
 
 export IMAGE_VERSION=$COMMIT_SHA
-export APP_NAME=$GITHUB_REPOSITORY
+export APP_NAME=$APP_NAME
 
-echo "Deploying - $GITHUB_REPOSITORY:$COMMIT_SHA"
+echo "Deploying - $APP_NAME:$COMMIT_SHA"
 
 cat ./k8s/deployment.yaml | envsubst | kubectl apply -f -
